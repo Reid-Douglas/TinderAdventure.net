530a964f70bde2e484d8438ac699ae849c047752
reiddoug814 <reiddoug814@SDS-L-231HC63>
2023-03-27 14:50:01 -0400

    Add project files.

---
+++ TinderAdventure.net/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TinderAdventure.net
+{
+    partial class tinderAdventure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tinderAdventure));
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // option1Button
+            // 
+            this.option1Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("option1Button.BackgroundImage")));
+            this.option1Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option1Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option1Button.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.option1Button.Location = new System.Drawing.Point(4, 251);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(75, 23);
+            this.option1Button.TabIndex = 0;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("option2Button.BackgroundImage")));
+            this.option2Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.option2Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option2Button.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.option2Button.Location = new System.Drawing.Point(4, 298);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(75, 23);
+            this.option2Button.TabIndex = 1;
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(1, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(71, 13);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "OutputLabel1";
+            // 
+            // option1Label
+            // 
+            this.option1Label.AutoSize = true;
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(70, 256);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(70, 13);
+            this.option1Label.TabIndex = 3;
+            this.option1Label.Text = "Option1Label";
+            this.option1Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // option2Label
+            // 
+            this.option2Label.AutoSize = true;
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(70, 303);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(70, 13);
+            this.option2Label.TabIndex = 4;
+            this.option2Label.Text = "Option2Label";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 63);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tinderAdventure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(307, 404);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "tinderAdventure";
+            this.Text = "Tinder Adventure";
+            this.Load += new System.EventHandler(this.tinderAdventure_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TinderAdventure.net/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TinderAdventure.net
+{
+    public partial class tinderAdventure : Form
+    {
+        // Tracks what page of the story the user is at
+        int page = 1;
+
+        Random randGen = new Random();
+        int randomValue;
+
+        public tinderAdventure()
+        {
+            InitializeComponent();
+            // Display initial message and options
+            outputLabel.Text = "You downloaded Tinder as a joke but have now found yourself\n very lonely. Do you want to start your adventure with Tinder?";
+
+            option1Label.Text = "Yes";
+            option2Label.Text = "No";
+        }
+
+        private void tinderAdventure_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// Check what page we are currently on, and then flip
+            /// to the page you need to go to if you selected option 1
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 10;
+            }
+            else if (page == 10)
+            {
+                page = 12;
+            }
+            else if (page == 12)
+            {
+                page = 14;
+            }
+            if (page == 14)
+            {
+                page = 17;
+            }
+            else if (page == 17)
+            {
+                page = 19;
+            }
+            else if (page == 19)
+            {
+                page = 99;
+            }
+            else if (page == 16)
+            {
+                page = 17;
+            }
+            else if (page == 17)
+            {
+                page = 19;
+            }
+            else if (page == 19)
+            {
+                page = 99;
+            }
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You downloaded Tinder as a joke but have now found yourself very lonely. Do you want to start your adventure with Tinder?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 2:
+                    outputLabel.Text = "You matched with Savannah! Do you take her on a date?";
+
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 4:
+                    outputLabel.Text = "Where will you take her? ";
+
+                    option1Label.Text = "East side marios";
+                    option2Label.Text = "McDonalds play place";
+                    break;
+                case 6:
+                    outputLabel.Text = "She shows up, but comes 10 minutes late.";
+
+                    option1Label.Text = "Continue with date";
+                    option2Label.Text = "Ask why she's late";
+                    break;
+                case 8:
+                    outputLabel.Text = "You sit down at a table and order some drinks, she begins to\n complain about her mom and you are intrigued about it";
+
+                    option1Label.Text = "Agree with her";
+                    option2Label.Text = "Tell her to stop talking";
+                    break;
+                case 10:
+                    outputLabel.Text = "Savannah finds it nice that your listening to her and asks if you\n want to go back to her place";
+
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    break;
+                case 12:
+                    outputLabel.Text = "Savannah respects you wishes, but seems infuriated by your\n answer, she then asks why?";
+
+                    option1Label.Text = "Don't like her";
+                    option2Label.Text = "Not ready";
+                    break;
+                case 14:
+                    outputLabel.Text = "Savannah gets sad hearing this and files a restraining order on you, she says she never wants to hear from you again, she picks up a butter knife and throws it at you, impaling your lungs and killing you. Play Again?";
+
+                    option1Label.Text = "Don't like her";
+                    option2Label.Text = "Not ready";
+                    break;
+                case 99:
+                    outputLabel.Text = "Play again";
+
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 99;
+            }
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 99;
+            }
+            else if (page == 4)
+            {
+                randomValue = randGen.Next(1, 101);
+                if (randomValue > 20)
+                {
+                    page = 6;
+                }
+                else
+                {
+                    page = 7;
+                }
+            }
+            else if (page == 7)
+            {
+                page = 99;
+            }
+                else if (page == 6)
+                {
+                    page = 9;
+                }
+                else if (page == 9)
+                {
+                    page = 99;
+                }
+                else if (page == 8)
+                {
+                    page = 11;
+                }
+                else if (page == 11)
+                {
+                    page = 99;
+                }
+                else if (page == 10)
+                {
+                    page = 13;
+                }
+                else if (page == 13)
+                {
+                    page = 99;
+                }
+                else if (page == 12)
+                {
+                    page = 15;
+                }
+                else if (page == 15)
+                {
+                    page = 99;
+                }
+                else if (page == 4)
+                {
+                    page = 16;
+                }
+                else if (page == 16)
+                {
+                    page = 18;
+                }
+                else if (page == 18)
+                {
+                    page = 99;
+                }
+                else if (page == 17)
+                {
+                    page = 20;
+                }
+                else if (page == 20)
+                {
+                    page = 99;
+                }
+                
+
+            
+
+            }
+        }
+    }
